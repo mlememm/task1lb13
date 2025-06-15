391c0db68e1fd2243b32a2304554eaa55c9a4441
mlememm <yun.viviyet@gmail.com>
2025-06-16 02:38:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class SettingsManager
+{
+    private static SettingsManager _instance;
+    private static readonly object _lock = new object();
+    public string Theme { get; set; }
+    public string Language { get; set; }
+    private SettingsManager()
+    {
+        Theme = "Light";
+        Language = "Ukrainian";
+    }
+    public static SettingsManager Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                    _instance = new SettingsManager();
+                return _instance;
+            }
+        }
+    }
+    public void PrintSettings()
+    {
+        Console.WriteLine($"Theme: {Theme}, Language: {Language}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        var settings1 = SettingsManager.Instance;
+        settings1.PrintSettings();
+        settings1.Theme = "Dark";
+        settings1.Language = "English";
+
+        var settings2 = SettingsManager.Instance;
+        settings2.PrintSettings();
+    }
+}
